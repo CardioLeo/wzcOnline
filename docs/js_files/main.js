fa6c2b6d2281ca5672524d35622b7ebb55ad8abb
echocslsk <CardioLeo@gmail.com>
2022-09-15 05:32:39 -0600

    moves all files into a docs file, hopefully will fix build error

---
+++ docs/js_files/main.js
@@ -0,0 +1,22 @@
+// Author: William Z Chadwick
+// Date Created: 09/14/2022
+// Date Modified: 09/14/2022
+// Description: A place for JavaScript code for my website updates during Promineo Tech Front End Bootcamp Week 7;
+//
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+	coll[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if (content.style.display === "block") {
+			content.style.display = "none";
+		} else {
+			content.style.display = "block";
+		}
+	});
+}
+
+// the above code was taken from (though deliberately typed out for practice) the following source: https://www.w3schools.com/howto/howto_js_collapsible.asp
